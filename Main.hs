b533c3b12c7fddbe7ec3dc8c0a1dd5a1162871b5
acf16408ip <acf16408ip@es1.abci.local>
2024-05-06 17:17:41 +0900

    cannot readFromFile

--- Main.hs
+++ Main.hs
@@ -8,6 +8,40 @@ module Main where
 import Control.Monad (when)
 import Torch
 
+-- from base
+import GHC.Generics
+import System.IO
+import System.Exit (exitFailure)
+-- from bytestring
+import Data.ByteString (ByteString, hGetSome, empty)
+import qualified Data.ByteString.Lazy as BL
+-- from cassava
+import Data.Csv.Incremental
+import Data.Csv (FromRecord, ToRecord)
+
+data WeatherData = WeatherData
+  { date :: !ByteString
+  , daily_mean_temperature  :: !Double
+  } deriving (Show, Eq, Generic)
+
+instance FromRecord WeatherData
+instance ToRecord WeatherData
+
+feed :: (ByteString -> Parser WeatherData) -> Handle -> IO (Parser WeatherData)
+feed k csvFile = do
+  hIsEOF csvFile >>= \case
+    True  -> return $ k empty
+    False -> k <$> hGetSome csvFile 4096
+
+readFromFile :: IO ()
+readFromFile = do
+  withFile "train.csv" ReadMode $ \ csvFile -> do
+    let loop !_ (Fail _ errMsg) = do putStrLn errMsg; exitFailure
+        loop acc (Many rs k)    = loop (acc <> rs) =<< feed k csvFile
+        loop acc (Done rs)      = print (acc <> rs)
+    loop [] (decode NoHeader)
+
+
 model :: Linear -> Tensor -> Tensor
 model state input = squeezeAll $ linear state input
 
@@ -24,6 +58,8 @@ printParams trained = do
 
 main :: IO ()
 main = do
+  readFromFile
+
   init <- sample $ LinearSpec {in_features = numFeatures, out_features = 1}
   randGen <- defaultRNG
   printParams init
