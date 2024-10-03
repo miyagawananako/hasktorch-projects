51e81f90f6d9bbc8df17c8eb888bb6ee20a232ee
acf16408ip <acf16408ip@es1.abci.local>
2024-10-03 17:10:47 +0900

    RNNのメモ

---
+++ app/rnn/Main.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Elman where
+
+import Control.Monad.State.Strict
+import Data.List (foldl', intersperse, scanl')
+import RecurrentLayer
+import Torch
+
+data ElmanSpec = ElmanSpec {in_features :: Int, hidden_features :: Int}
+
+data ElmanCell = ElmanCell
+  { input_weight :: Parameter,
+    hidden_weight :: Parameter,
+    bias :: Parameter
+  }
+
+-- RecurrentCell型クラスのインスタンスElmanCell
+-- {..}はElmanCellのフィールドのパターンマッチング
+-- nextState :: ElmanCell -> Tensor -> Tensor -> Tensor
+-- gate関数が呼び出されています。この関数は、入力テンソル、隠れ状態テンソル、活性化関数（ここではTorch.tanh）、およびElmanCellのフィールド（input_weight、hidden_weight、bias）を受け取り、次の隠れ状態を計算します。Torch.tanhは、双曲線正接関数であり、ニューラルネットワークの活性化関数としてよく使用されます。
+instance RecurrentCell ElmanCell where
+  nextState ElmanCell {..} input hidden =
+    gate input hidden Torch.tanh input_weight hidden_weight bias
+
+-- =<<はHaskellにおけるモナディックな結合演算子の一つで、bind演算子とも呼ばれます。この演算子は、モナドの文脈で関数を適用するために使用されます。具体的には、モナドの値を取り、その値を引数として関数に渡し、結果として新しいモナドの値を返します。
+-- Randomizable型クラスのインスタンス、ElmanSpecとElmanCell
+-- sample :: ElmanSpec -> IO ElmanCell
+-- randnIO' [in_features, hidden_features]は、指定された形状のランダムなテンソルを生成します。
+-- makeIndependentは、そのテンソルを独立したテンソルに変換します。
+-- 同様に、隠れ層から隠れ層への重みテンソルを生成し、w_hhに格納します。
+-- バイアステンソルを生成し、bに格納します。
+-- このようにして、ElmanSpecからElmanCellをランダムに生成するための具体的な実装が提供されます。これは、ニューラルネットワークの初期化やランダムな重みの設定に非常に有用です。
+instance Randomizable ElmanSpec ElmanCell where
+  sample ElmanSpec {..} = do
+    w_ih <- makeIndependent =<< randnIO' [in_features, hidden_features]
+    w_hh <- makeIndependent =<< randnIO' [hidden_features, hidden_features]
+    b <- makeIndependent =<< randnIO' [1, hidden_features]
+    return $ ElmanCell w_ih w_hh b
+
+instance Parameterized ElmanCell where
+  flattenParameters ElmanCell {..} = [input_weight, hidden_weight, bias] -- ElmanCellのパラメータをリストに変換している
+  _replaceParameters _ = do
+    input_weight <- nextParameter  -- nextParameter関数を使用して新しいパラメータを生成する
+    hidden_weight <- nextParameter
+    bias <- nextParameter
+    return $ ElmanCell {..}
+
+instance Show ElmanCell where
+  show ElmanCell {..} =
+    (show input_weight) ++ "\n"
+      ++ (show hidden_weight)
+      ++ "\n"
+      ++ (show bias)
+      ++ "\n"
---
+++ app/rnn/RNN.hs
@@ -0,0 +1,183 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
+{- Throuout the comments, a tensor of shape [a,b,..] is written as <a,b,...> -}
+
+module Torch.Layer.RNN (
+  RnnHypParams(..)
+  , RnnParams(..)
+  , singleRnnLayer
+  , rnnLayers
+  , InitialStatesHypParams(..)
+  , InitialStatesParams(..)
+  ) where 
+
+import Prelude hiding   (tanh) 
+import GHC.Generics              --base
+import Data.Function    ((&))    --base
+import Data.Maybe       (isJust) --base
+import Data.List        (scanl',foldl',scanr) --base
+import Control.Monad    (forM,unless)     --base
+import System.IO.Unsafe (unsafePerformIO) --base
+--hasktorch
+import Torch.Tensor      (Tensor(..),shape,select,sliceDim,reshape)
+import Torch.Functional  (Dim(..),add,sigmoid,cat,stack,dropout,transpose)
+import Torch.Device      (Device(..))
+import Torch.NN          (Parameterized(..),Randomizable(..),Parameter,sample)
+import Torch.Autograd    (makeIndependent)
+--hasktorch-tools
+import Torch.Tensor.Util (unstack)
+import Torch.Tensor.TensorFactories (randintIO')
+import Torch.Layer.Linear (LinearHypParams(..),LinearParams(..),linearLayer)
+import Torch.Layer.NonLinear (ActName(..),decodeAct)
+
+(.->) :: a -> (a -> b) -> b
+(.->) = (&)
+
+data RnnHypParams = RnnHypParams {
+  dev :: Device
+  , bidirectional :: Bool -- ^ True if BiLSTM, False otherwise
+  , inputSize :: Int  -- ^ The number of expected features in the input x（入力テンソルの特徴量の数）
+  , hiddenSize :: Int -- ^ The number of features in the hidden state h
+  , numLayers :: Int  -- ^ Number of recurrent layers（RNNの再帰層の数）
+  , hasBias :: Bool   -- ^ If False, then the layer does not use bias weights b_ih and b_hh.
+  -- , batch_first :: Bool -- ^ If True, then the input and output tensors are provided as (batch, seq, feature) instead of (seq, batch, feature).
+  } deriving (Eq, Show)
+
+newtype SingleRnnParams = SingleRnnParams {
+    rnnGate :: LinearParams
+    } deriving (Show, Generic)
+instance Parameterized SingleRnnParams
+
+-- cat関数を使用して、入力テンソルxtと隠れ状態テンソルhtを次元0（行方向）に沿って連結しています。これにより、2つのテンソルが1つのテンソルに結合されます。
+-- 連結されたテンソルに対して、rnnGateを使用した線形変換を適用しています。linearLayer関数は、線形変換を実行する関数であり、rnnGateはそのパラメータを提供します。
+-- このようにして、rnnCell関数は、現在の隠れ状態と入力を受け取り、それらを連結して線形変換を適用することで、次の隠れ状態を計算します。これは、RNNセルの基本的な動作を実装するための重要なステップです。
+rnnCell :: SingleRnnParams 
+  -> Tensor -- ^ ht of shape <hDim>   （<hDim>は隠れ状態の次元数を示す）
+  -> Tensor -- ^ xt of shape <iDim/oDim>
+  -> Tensor -- ^ ht' of shape <hDim>
+rnnCell SingleRnnParams{..} ht xt = linearLayer rnnGate $ cat (Dim 0) [xt,ht]
+
+-- | inputのlistから、(cellState,hiddenState=output)のリストを返す
+-- | （rnLayersのサブルーチン。外部から使う想定ではない）
+-- | scanl'の型メモ :: ((h,c) -> input -> (h',c')) -> (h0,c0) -> [input] -> [(hi,ci)]
+singleRnnLayer :: Bool -- ^ bidirectional (True if bidirectioal, False otherwise)
+  -> Int               -- ^ stateDim (=hDim)
+  -> ActName           -- ^ actname (the name of nonlinear function)
+  -> SingleRnnParams   -- ^ singleRnnParams
+  -> Tensor            -- ^ h0: <1,hDim> for one-directional and <2,hDim> for BiLSTM
+  -> Tensor            -- ^ inputs: <seqLen,iDim/oDim> for the 1st-layer/the rest
+  -> (Tensor,Tensor)   -- ^ an output pair (<seqLen,D*oDim>,<D*oDim>)
+singleRnnLayer bidirectional stateDim actname singleRnnParams h0 inputs = unsafePerformIO $ do
+  let h0shape = shape h0
+      [seqLen,_] = shape inputs
+      d = if bidirectional then 2 else 1
+      actf = decodeAct actname
+  unless (h0shape == [d,stateDim]) $ ioError $ userError $ "illegal shape of h0: " ++ (show h0shape) 
+  if bidirectional -- check the well-formedness of the shapes of h0 and c0
+    then do -- the case of BiRNN
+      let h0f = select 0 0 h0 -- | pick the first h0 for the forward cells
+          h0b = select 0 1 h0 -- | pick the second h0 for the backward cells
+          hsForward = inputs  -- | <seqLen,iDim/oDim> 
+            .-> unstack       -- | [<iDim/oDim>] of length seqLen
+            .-> scanl' (rnnCell singleRnnParams) h0f -- | [<hDim>] of length seqLen+1
+            .-> tail          -- | [<hDim>] of length seqLen (by removing h0f)
+            .-> stack (Dim 0) -- | <seqLen, hDim>
+          hsBackward = inputs -- | <seqLen,iDim/oDim> 
+            .-> unstack       -- | [<iDim/oDim>] of length seqLen
+            .-> scanr (flip $ rnnCell singleRnnParams) h0b -- | [<hDim>] of length seqLen+1
+            .-> init          -- | [<hDim>] of length seqLen (by removing h0b)
+            .-> stack (Dim 0) -- | <seqLen, hDim>
+          output = [hsForward, hsBackward]   -- | [<seqLen, hDim>] of length 2
+            .-> stack (Dim 0)                -- | <2, seqLen, hDim>
+            .-> actf                         -- | <2, seqLen, hDim> ??
+            .-> transpose (Dim 0) (Dim 1)    -- | <seqLen, 2, hDim>
+            .-> reshape [seqLen, 2*stateDim] -- | <seqLen, 2*hDim>
+          hLast = output                           -- | <seqLen, 2*hDim>
+            .-> sliceDim 0 (seqLen-1) seqLen 1     -- | <1, 2*hDim>
+            .-> (\o -> reshape (tail $ shape o) o) -- | <2*hDim> 
+      return (output, hLast)
+    else do -- the case of RNN
+      let h0f = select 0 0 h0
+          output = inputs
+            .-> unstack          -- | [<iDim/oDim>] of length seqLen
+            .-> scanl' (rnnCell singleRnnParams) h0f -- | [<hDim>] of length seqLen+1
+            .-> tail             -- | [<hDim>] of length seqLen (by removing h0)
+            .-> stack (Dim 0)    -- | <seqLen, hDim>
+            .-> actf             -- | <seqLen, hDim> ??
+          hLast = output                           -- | <seqLen, hDim>
+            .-> sliceDim 0 (seqLen-1) seqLen 1     -- | <1, hDim>
+            .-> (\o -> reshape (tail $ shape o) o) -- | <hDim>
+      return (output, hLast)
+
+data RnnParams = RnnParams {
+  firstRnnParams :: SingleRnnParams    -- ^ a model for the first RNN layer
+  , restRnnParams :: [SingleRnnParams] -- ^ models for the rest of RNN layers
+  } deriving (Show, Generic)
+instance Parameterized RnnParams
+
+instance Randomizable RnnHypParams RnnParams where
+  sample RnnHypParams{..} = do
+    let xDim = inputSize
+        hDim = hiddenSize
+        xh1Dim = xDim + hDim
+        d = if bidirectional then 2 else 1
+        xh2Dim = (d * hDim) + hDim
+    RnnParams
+      <$> (SingleRnnParams <$> sample (LinearHypParams dev hasBias xh1Dim hDim)) -- gate
+      <*> forM [2..numLayers] (\_ ->
+        SingleRnnParams <$> sample (LinearHypParams dev hasBias xh2Dim hDim)
+        )
+
+-- | The main function for RNN layers
+rnnLayers :: RnnParams -- ^ parameters (=model)
+  -> ActName         -- ^ name of nonlinear function
+  -> Maybe Double    -- ^ introduces a Dropout layer on the outputs of each LSTM layer except the last layer, with dropout probability equal to dropout.
+  -> Tensor          -- ^ an initial tensor: <D*numLayers,hDim>
+  -> Tensor          -- ^ an input tensor <seqLen,iDim>
+  -> (Tensor,Tensor) -- ^ an output of (<seqLen,D*oDim>,(<D*numLayers,oDim>,<D*numLayers,cDim>))
+rnnLayers RnnParams{..} actname dropoutProb h0 inputs = unsafePerformIO $ do
+  let numLayers = length restRnnParams + 1
+      (dnumLayers:(hiddenSize:_)) = shape h0
+  unless (dnumLayers == numLayers * 2 || dnumLayers == numLayers) $ 
+    ioError $ userError $ "illegal shape of h0: dnumLayers = " ++ (show dnumLayers) ++ "\nnumLayers = " ++ (show numLayers) 
+  let bidirectional | dnumLayers == numLayers * 2 = True
+                    | dnumLayers == numLayers = False
+                    | otherwise = False -- Unexpected
+      d = if bidirectional then 2 else 1
+      (h0h:h0t) = [sliceDim 0 (d*i) (d*(i+1)) 1 h0 | i <- [0..numLayers]]
+      firstLayer = singleRnnLayer bidirectional hiddenSize actname firstRnnParams h0h
+      restOfLayers = map (uncurry $ singleRnnLayer bidirectional hiddenSize actname) $ zip restRnnParams h0t 
+      dropoutLayer = case dropoutProb of
+                       Just prob -> unsafePerformIO . (dropout prob True)
+                       Nothing -> id
+      stackedLayers = \inputTensor -> 
+                        scanr
+                          (\nextLayer ohc -> nextLayer $ dropoutLayer $ fst ohc)
+                          (firstLayer inputTensor) -- (<seqLen,D*oDim>,(<D*oDim>,<D*cDim>))
+                          restOfLayers -- 
+      (outputList, hn) = inputs -- | <seqLen,iDim>
+        .-> stackedLayers       -- | [(<seqLen, D*oDim>,<D*oDim>)] of length numLayers
+        .-> unzip               -- | ([<seqLen, D*oDim>] of length numLayers, [(<D*oDim>,<D*cDim>)] of length numLayers)
+      output = head outputList  -- | [<seqLen, D*oDim>] of length numLayers
+      rh = hn                                      -- | 
+           .-> stack (Dim 0)                       -- | <numLayers, D*oDim/cDim>
+           .-> reshape [d * numLayers, hiddenSize] -- | <D*numLayers, oDim/cDim>
+  return (output, rh)
+
+data InitialStatesHypParams = InitialStatesHypParams {
+  dev :: Device
+  , bidirectional :: Bool
+  , hiddenSize :: Int
+  , numLayers :: Int
+  } deriving (Eq, Show)
+
+newtype InitialStatesParams = InitialStatesParams {
+  h0s :: Parameter 
+  } deriving (Show, Generic)
+instance Parameterized InitialStatesParams
+
+instance Randomizable InitialStatesHypParams InitialStatesParams where
+  sample InitialStatesHypParams{..} = 
+    InitialStatesParams
+      <$> (makeIndependent =<< randintIO' dev (-1) 1 [(if bidirectional then 2 else 1) * numLayers, hiddenSize])
