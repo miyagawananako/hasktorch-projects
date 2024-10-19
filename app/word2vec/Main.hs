576485f045cf4308f4603ff910566953395b8169
acf16408ip <acf16408ip@es1.abci.local>
2024-10-20 02:23:55 +0900

    word2vec

--- app/word2vec/Main.hs
+++ app/word2vec/Main.hs
@@ -12,17 +12,17 @@ import qualified Data.ByteString.Lazy as B -- add bytestring to dependencies in 
 import Data.Word (Word8)
 import qualified Data.Map.Strict as M -- add containers to dependencies in package.yaml
 import Data.List (nub)
+-- import qualified Data.HashSet as HashSet
+import qualified Data.Set as Set
 
 import Torch.Autograd (makeIndependent, toDependent)
-import Torch.Functional (embedding')
+import Torch.Functional (embedding', Dim(..), mseLoss, softmax, stack, squeezeDim, relu, matmul, split, transpose)
 import Torch.NN (Parameterized(..), Parameter, linear)
 import Torch.Serialize (saveParams, loadParams)
 import Torch.Tensor (Tensor, asTensor, shape, asValue)
 import Torch.TensorFactories (eye', zeros', full)
-import Torch.Functional (Dim(..), mseLoss, softmax, stack)
 import Torch.Optim        (foldLoop, GD(..), Loss, runStep, LearningRate)
 import Torch.NN (Linear(..), sample, LinearSpec(..))
-import Torch.Functional (relu, matmul)
 import Torch.Control      (mapAccumM)
 import Torch.TensorOptions (defaultOpts)
 
@@ -30,8 +30,8 @@ import System.Random.Shuffle (shuffleM)
 import ML.Exp.Chart   (drawLearningCurve) --nlp-tools
 
 -- your text data (try small data first)
--- textFilePath = "app/word2vec/data/sample.txt"
-textFilePath = "app/word2vec/data/sample_mini.txt"
+textFilePath = "app/word2vec/data/sample.txt"
+-- textFilePath = "app/word2vec/data/sample_mini.txt"
 modelPath =  "app/word2vec/data/sample_embedding.params"
 wordLstPath = "app/word2vec/data/sample_wordlst.txt"
 
@@ -44,18 +44,9 @@ data Embedding = Embedding {
     wordEmbedding :: Parameter
   } deriving (Show, Generic, Parameterized)
 
--- 使わない
-data MLP = MLP
-  { layers :: [Linear],  -- 入力テンソルに対して線形変換を適用する役割を持つ
-    nonlinearity :: Tensor -> Tensor  -- 非線形活性化関数
-  } deriving (Generic, Parameterized)
-
--- 使わない
--- Probably you should include model and Embedding in the same data class.
 data Model = Model {
-    mlp :: MLP
-    -- w_in :: Embedding,
-    -- w_out :: Embedding,
+    w_in :: Embedding,
+    w_out :: Embedding
   } deriving (Generic, Parameterized)
 
 isUnnecessaryChar :: 
@@ -105,10 +96,10 @@ vecBinaryAddition :: Tensor -> Tensor -> Tensor
 vecBinaryAddition vec1 vec2 = vec1 + vec2
 
 -- CBOW（input: 周辺4単語, output: 中心単語）
-initDataSets :: [[B.ByteString]] -> [B.ByteString] -> [(Tensor, Tensor)]
-initDataSets wordLines wordlst = pairs
-  where
-      dictLength = Prelude.length wordlst
+-- inputのTensor を 4*len(wordlst)にした
+initDataSets :: [[B.ByteString]] -> [B.ByteString] -> IO [(Tensor, Tensor)]
+initDataSets wordLines wordlst = do
+  let dictLength = Prelude.length wordlst
       wordToIndex = wordToIndexFactory $ nub wordlst  -- indexを生成
       input = concatMap createInputPairs wordlst
       output = concatMap createOutputPairs wordlst
@@ -117,31 +108,29 @@ initDataSets wordLines wordlst = pairs
         let indices = [wordToIndex word - 1, wordToIndex word + 1, wordToIndex word - 2, wordToIndex word + 2]
             validIndices = filter (\i -> i >= 0 && i < Prelude.length wordlst) indices
             vectors = map (\i -> oneHotEncode (wordToIndex (wordlst !! i)) dictLength) validIndices
-        in [foldl1 vecBinaryAddition vectors]
+        in [stack (Dim 0) vectors]   -- []リストなのか危うい。リスト外したほうがいいかも
       createOutputPairs word = [oneHotEncode (wordToIndex word) dictLength]
-
-
--- MLPの初期化（これが違う）
-initMLP :: IO MLP
-initMLP = do
-  layer1 <- sample $ LinearSpec 9 64
-  layer2 <- sample $ LinearSpec 64 370  -- 370は単語数
-  let layers = [layer1, layer2]
-  let nonlinearity = relu  -- 入力値が0以下の場合は0より上の場合には出力値が入力値と同じ値となる関数
-  return $ MLP layers nonlinearity
+  return pairs
 
 -- フォワードパスの実装
-predict :: Model -> Tensor -> Embedding -> IO Tensor
-predict model input embedding = do
-  let emb = wordEmbedding embedding  -- 埋め込み行列を取得
-  -- print emb 出力 IndependentTensor {toDependent = Tensor Float [370,9] [[ 1.0000   ,  0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.0000],
-  -- print input  -- 出力できない  Not implemented for Tensor-typeの部分だった
-  let embeddedInput = matmul input (toDependent emb)  -- 入力テンソルと埋め込み行列の行列乗算を行う（toDependentは通常テンソルに戻す）
-  -- print embeddedInput  -- 出力されず　→できた
-  let mlpLayers = layers (mlp model)  -- モデルのMLPレイヤーを取得
-  let nonlin = nonlinearity (mlp model)  -- 非線形活性化関数を取得
-  let output = foldl (\acc layer -> nonlin (linear layer acc)) embeddedInput mlpLayers  -- mlpLayers（リスト）の各要素のmlpレイヤーを適用し、非線形変換を行う。accの初期値はembeddedInput
-  return output
+-- inputのTensor を [4*len(wordlst), batchSize]にする
+predict :: Model -> Tensor -> IO Tensor
+predict model input = do
+  let emb_in = wordEmbedding (w_in model)
+  -- print (shape input)  -- [32,4,370]
+  -- print (shape (toDependent emb_in))  -- [370,9]
+  let embeddedInputs = split 1 (Dim 1) (matmul input (toDependent emb_in))
+  let sumTensor = foldl1 vecBinaryAddition embeddedInputs -- ここの処理を変える
+  -- print (shape sumTensor)  -- [32,1,9] TODO: ここの形を[32,1,9]に変えたい dim0からdim1に変えた
+  let avgTensor = sumTensor / 4
+  -- print (shape avgTensor)  -- [32,1,9]
+  let nonlin = softmax (Dim 0)
+  let emb_out = wordEmbedding (w_out model)
+  -- print (shape avgTensor)  -- [32,1,9]
+  -- print (shape (toDependent emb_out)) -- [370,9]
+  let output = nonlin (matmul avgTensor (transpose (Dim 0) (Dim 1) (toDependent emb_out)))  -- (32x9 and 9×370)
+  -- let output = foldl (\acc layer -> nonlin (linear layer acc)) embeddedInput mlpLayers  -- mlpLayers（リスト）の各要素のmlpレイヤーを適用し、非線形変換を行う。accの初期値はembeddedInput
+  return output  -- おそらく[32,1,370]
 
 main :: IO ()
 main = do
@@ -149,59 +138,52 @@ main = do
   texts <- B.readFile textFilePath  -- texts :: B.Internal.ByteString
 
   -- create word lst (unique)
-  let wordLines = preprocess texts -- wordLines :: [[B.ByteString]]
-      wordlst = nub $ concat wordLines  -- wordlst :: [B.ByteString]
-      wordToIndex = wordToIndexFactory wordlst  -- wordToIndex :: B.ByteString -> Int
-  -- print wordlst
+  let wordLines' = preprocess texts -- wordLines :: [[B.ByteString]]
+  let (wordLines, _) = splitAt (length wordLines' * 1 `div` 100) wordLines'
+  let wordlst = Set.toList . Set.fromList . concat $ wordLines
+  let wordToIndex = wordToIndexFactory wordlst  -- wordToIndex :: B.ByteString -> Int
 
   -- create embedding(wordDim × wordNum)
   let embsddingSpec = EmbeddingSpec {wordNum = length wordlst, wordDim = 9} -- emsddingSpec :: EmbeddingSpec
   wordEmb <- makeIndependent $ toyEmbedding embsddingSpec -- wordEmb :: IndependentTensor
-  let w_in = Embedding { wordEmbedding = wordEmb } -- w_in :: Embedding
-
-  mlp <- initMLP
-  let initModel = Model mlp
+  let initW_in = Embedding { wordEmbedding = wordEmb } -- w_in :: Embedding
+      initW_out = Embedding { wordEmbedding = wordEmb }
+      initModel = Model { w_in = initW_in, w_out = initW_out }
 
   -- trainingData :: [(Tensor, Tensor)]
-  let trainingData = initDataSets wordLines wordlst
-  -- print $ trainingData !! 8
+  trainingData' <- initDataSets wordLines wordlst
+  let trainingData = drop 2 (take (length trainingData' - 2) trainingData')  -- 最初と最後だけ削除
+  -- print $ trainingData !! 8 -- ここの出力がなかなか出てこない（データ10%だと大丈夫だった）
 
   let optimizer = GD
-      numIters = ((length trainingData) `div` batchsize)
-      learningRate = asTensor [0.01::Float]
-      batchsize = 32
+      numIters = 5
+      learningRate = asTensor [0.1::Float]
+      batchsize = 2048
 
-  initRandamTrainData <- shuffleM trainingData
+  -- train 1個ずつ出力していく、確信を増やしていく。とりあえず直す。
+  (trainedModel', _, losses') <- foldLoop (initModel, optimizer, []) numIters $ \(model', opt, lossesList) i -> do
+    initRandamTrainData <- shuffleM trainingData
+    ((trainedModel, _, _, _),losses) <- mapAccumM [1..((length trainingData) `div` batchsize)] (model', opt, initRandamTrainData, 0) $ \epoc (model, opt, randamTrainData, index) -> do
+      let batchIndex = (index - 1) * batchsize
+      let dataList = take batchsize $ drop batchIndex randamTrainData -- [(Tensor, Tensor)]
+      let (input, target) = unzip dataList
+      output <- predict model (stack (Dim 0) input)
+      let loss = (mseLoss (stack (Dim 0) target) (squeezeDim 1 output)) / (asTensor (length dataList))  -- loss :: Tensor lossはバッチサイズで割るべきなのでは。→割った
+      let newIndex = index + 1
+      print index
+      (newModel, _) <- runStep model optimizer loss learningRate  -- loss :: Torch.Optim.Loss（バッチごとに更新する）
+      let lossValue = (asValue loss)::Float
+      return ((newModel, opt, randamTrainData, newIndex), lossValue)  --更新されたモデルと損失値を返す, embも渡す？？？
+    let avgLoss = sum losses / fromIntegral (length losses)
+    -- print (w_in model)  -- ちゃんと更新はされている
+    pure (trainedModel, opt,  lossesList ++ [avgLoss]) 
 
-  print "before training"
+  drawLearningCurve "/home/acf16408ip/hasktorch-projects/app/word2vec/graph/learning_curve.png" "Learning Curve" [("",reverse losses')]
 
-  -- train（エラーを吐く部分）  1個ずつ出力していく、確信を増やしていく。とりあえず直す。
-  ((trainedModel, _, _, _),losses) <- mapAccumM [1..numIters] (initModel, optimizer, initRandamTrainData, 0) $ \epoc (model, opt, randamTrainData, index) -> do  -- 各エポックでモデルを更新し、損失を蓄積。
-    let batchIndex = (index - 1) * batchsize
-    let dataList = take batchsize $ drop batchIndex randamTrainData -- [(Tensor, Tensor)]
-    let (input, target) = unzip dataList
-    -- print $ shape (head input)  -- input::[Tensor]  [370]と出力された
-    -- print $ length input  -- 32
-    -- print $ shape target[0]  -- [Tensor]
-    output <- predict model (stack (Dim 0) input) w_in  -- このw_inが更新されるべきじゃないか
-    let loss = mseLoss (stack (Dim 0) target) output  -- loss :: Tensor
-    --  print lossの結果、　The size of tensor a (32) must match the size of tensor b (370) at non-singleton dimension 1
-    let newIndex = index + 1
-    -- let lossTensor = asTensor loss
-    -- let learningRateTensor = asTensor learningRate
-    (newModel, _) <- runStep model optimizer (loss :: Torch.Optim.Loss) learningRate  -- loss :: Torch.Optim.Loss
-    let lossValue = (asValue loss)::Float
-    return ((newModel, opt, randamTrainData, newIndex), lossValue)  --更新されたモデルと損失値を返す, embも渡す？？？
-
-  drawLearningCurve "/home/acf16408ip/hasktorch-projects/app/word2vec/graph/learning_curve.png" "Learning Curve" [("",reverse losses)]
-
-  print "after training"
-
-  -- trainedModelだけ保存しているのがおかしい。
-  -- w_inを取得したい。w_inはembedding'関数に入れて、単語の分散表現を獲得したい
+  -- w_inはembedding'関数に入れて、単語の分散表現を獲得したい
 
   -- save params
-  saveParams w_in modelPath
+  saveParams (w_in trainedModel') modelPath
   -- save word list
   B.writeFile wordLstPath (B.intercalate (B.pack $ encode "\n") wordlst)
   
